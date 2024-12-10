7b466ea52c9b2b321faffbd2aebb83869a7024ea
ParlarSec <parlarsec@proton.me>
2024-12-10 23:07:08 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,6 +2,3 @@ document.querySelector('.nav-toggle').addEventListener('click', () => {
     const navLinks = document.querySelector('.nav-links');
     navLinks.classList.toggle('active');
 });
-setTimeout(() => {
-    navLinks.style.display = 'none';
-}, 3);
