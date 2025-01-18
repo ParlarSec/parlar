57bb07b15fd0be2a8bd7fd1b452a913d410db188
PARLAR <parlarsec@proton.me>
2025-01-18 16:07:08 +0000

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -2,3 +2,19 @@ document.querySelector('.nav-toggle').addEventListener('click', () => {
     const navLinks = document.querySelector('.nav-links');
     navLinks.classList.toggle('active');
 });
+let lastScrollTop = 0;
+const navContainer = document.querySelector('.nav-container');
+
+window.addEventListener('scroll', () => {
+  const currentScroll = window.pageYOffset;
+
+  if (currentScroll > lastScrollTop) {
+    // Scrolling down, hide the nav container
+    navContainer.style.height = '0';
+  } else {
+    // Scrolling up, show the nav container
+    navContainer.style.height = '50px';
+  }
+
+  lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Mobile or negative scrolling
+});
